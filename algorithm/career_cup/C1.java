4aedff888214b867a553c5d6183d4f7e3391393b
teloon <telooon@gmail.com>
2012-07-20 20:51:07 +0800

    1st version of 2nd method

--- algorithm/career_cup/C1.java
+++ algorithm/career_cup/C1.java
@@ -3,8 +3,23 @@ import java.util.HashSet;
 public class C1{
     
     public boolean c1_1(String str){
-        HashSet<Character> hs = new HashSet<Character>();
-        for(int i=str.length()-1; i>=0; --i)
+        //condition all characters in str must be letters
+        int char_idx[] = new int[52];
+        for(int i=0; i<52; ++i) char_idx=0;
+        char ch;
+        for(int i=str.length()-1; i>=0; --i){
+            ch = str.charAt(i)
+            if Character.isUpperCase(ch){
+                if char_idx[ch-'A']>0:
+                    return true;
+                char_idx[ch-'A'] += 1;
+            }else{
+                if char_idx[ch-'a']>0:
+                    return true;
+                char_idx[ch-'a'] += 1;
+            }
+            return false;
+        }
             hs.add(str.charAt(i));
         if(hs.size() == str.length())
             return true;

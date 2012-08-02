af96e355c2f3d0afc756937d65438013e7c9b4dc
teloon <telooon@gmail.com>
2012-08-02 21:22:08 +0800

    add Queue

--- algorithm/career_cup/C3.java
+++ algorithm/career_cup/C3.java
@@ -2,18 +2,36 @@
 
 public class C3{
     public static void main(String[] args){
-        Stack stk = new Stack();
-        stk.push(1);
-        stk.pop();
-        stk.pop();
-        stk.push(1);
-        stk.push(2);
-        stk.push(3);
-        stk.push(4);
-        stk.push(5);
-        stk.push(6);
-        System.out.println(stk.top());
+        Queue q = new Queue();
+        System.out.println(stk.top().value);
         System.out.println(stk.size());
+    }
+}
+
+class Queue{
+    private int size;
+    private Node front;
+    private Node rear;
+
+    public Queue(){
+        front = new Node(-1);
+        rear = front;
+        size = 0;
+    }
+
+    public Node enqueue(int data){
+        rear.next = new Node(data);
+        rear = rear.next;
+        ++size;
+        return rear;
+    }
+
+    public Node dequeue(){
+        if(size <= 0) return null;
+        Node ret = front;
+        front = front.next;
+        --size;
+        return ret;
     }
 }
 
@@ -26,25 +44,25 @@ class Stack{
         size = 0;
     }
 
-    public boolean push(int data){
+    public Node push(int data){
         Node node = new Node(data);
         node.next = top;
         top = node;
         ++size;
-        return true;
+        return Node;
     }
 
-    public int pop(){
-        if(size <= 0) return -1;
-        int ret = top.data;
+    public Node pop(){
+        if(size <= 0) return null;
+        Node ret = top;
         top = top.next;
         --size;
         return ret;
     }
 
-    public int top(){
-        if(size <= 0) return -1;
-        return top.data;
+    public Node top(){
+        if(size <= 0) return null;
+        return top;
     }
 
     public int size(){
